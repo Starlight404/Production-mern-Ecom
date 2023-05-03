6c89fa6b13afc8af76cecd51a591f05b745470af
Rahul paul Chowdhury <78229611+Starlight404@users.noreply.github.com>
2023-05-03 23:34:20 +0600

    update port

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get("/", (req, res) => {
   res.send("<h1>Welcome to Ecom</h1>");
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`.bgCyan.white);
